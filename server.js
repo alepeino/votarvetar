29bf410c1af7111693818c85b86cabc3c7644d4a
Alejandro Peinó <alepeino@gmail.com>
2017-02-18 12:51:24 -0300

    Base

---
+++ server.js
@@ -0,0 +1,21 @@
+const env = require('env2')('.env');
+const express = require('express');
+const next = require('next');
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT;
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare()
+.then(() => {
+  const server = express();
+
+  // Rutas Next:
+  server.get('*', (req, res) => {
+    handle(req, res);
+  });
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+  });
+});
